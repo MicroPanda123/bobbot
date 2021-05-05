e44f9de890d844a717f1512f81d4adc4ff614371
MicroPanda123 <dawid12345.dawid@gmail.com>
2021-05-05 03:10:20 +0200

    removing temporary printing

--- bob.py
+++ bob.py
@@ -427,11 +427,8 @@ async def on_command_error(ctx, error):
 @client.event
 async def on_message(message):
     msg = message.content
-    print(msg)
     count_words(message.guild.id, message.author.id, msg)
     await client.process_commands(message)
 
-
-penis = "proncie"
 
 client.run(TOKEN)
