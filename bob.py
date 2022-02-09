7192b2584e702df351dc21a32812659006aaad09
MicroPanda123 <dawid12345.dawid@gmail.com>
2022-02-09 09:05:13 +0100

    name handling change

--- bob.py
+++ bob.py
@@ -2,13 +2,10 @@ import discord
 from discord.ext import commands
 from discord.ext.commands.errors import *
 import os
-#from dotenv import load_dotenv
 import openai
 
-#load_dotenv()
-#
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai_prompt = "You are Bob, you are a discord bot made to entertain people on this server. Be funny, but also little cranky if needed.\nHuman: "
+openai_prompt = "You are Bob, you are a discord bot made to entertain people on this server. Be funny, but also little cranky if needed.\n"
 client = commands.Bot(command_prefix='.')
 
 @client.event
@@ -23,10 +20,11 @@ async def on_message(message):
   msg = message.content
   if client.user.mentioned_in(message):
     print("Requesting response.")
-    print(msg)
+    prompt = openai_prompt + message.author.nick + ":" + msg.replace("<@!772573761941274654>", "", 1) + "\nAi:"
+    print(prompt)
     response = openai.Completion.create(
       engine="text-davinci-001",
-      prompt=openai_prompt + msg.replace("<@!772573761941274654>", "", 1) + "\nAi:",
+      prompt=prompt,
       max_tokens=64,
       stop="Human:"
     )
